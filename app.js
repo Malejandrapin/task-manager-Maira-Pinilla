3372e5da43ed86376c28fc39991303817c032862
Malejandrapin <dismajus@gmail.com>
2026-04-05 00:43:04 -0500

    Actualizando cambios

--- app.js
+++ app.js
@@ -24,6 +24,26 @@ function actualizarTareasCompletadas() {
     document.getElementById("cantidadCompletadas").textContent = completadas;
     console.log(`Tareas actualizadas:${completadas}/${total}`);
 }
-function rendarizarTareas(){
-    
+//Funcion para renderizar todas las tareas en pantalla a partir del array tareas, mostrar la tarea en pantalla
+function rendarizarTareas() {
+    listaTarea.innerHTML = "";
+    tareas.forEach((tarea) => {
+        //crea un elemento tipo lista en memoria
+        const li = document.createElement('li');
+        li.className = `tarjetaTarea ${tarea.done ? 'is-done' : ''} ${tarea.urgent ? 'is-urgent' : ''} `;
+
+        // Inserta los elementos HTML en la etiqueta li
+        li.innerHTML = `
+        <strong>${tarea.emoji}</strong> ${tarea.text}
+
+        <div class="acciones">
+          <button onclick="marcarHecha(${tarea.id})">✅ Hecha</button>
+        <button onclick="marcarUrgente(${tarea.id})">⚠️ Urgente</button>
+        <button onclick="eliminarTarea(${tarea.id})">🗑️ Eliminar</button>
+        </div>
+        `;
+    listaTarea.appendChild(li);
+});
+actualizarTareasCompletadas();
 }
+
